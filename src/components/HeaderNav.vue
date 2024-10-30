3d31357e01cd2630590405d4f734f408f29555e0
Diego Hernan Franco Torres <dfranco@vision.local>
2024-10-30 14:10:00 -0300

    ajuste header

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -2,7 +2,7 @@
     <header class="bg-gray-800 text-white">
   <nav class="container mx-auto flex items-center  p-4 gap-3">
 
-    <Icon :icon="'menu2'" :newClass="'size-5 m-24'" :color="'white'" @click="toogle"></Icon>
+    <Icon :icon="'menu2'" :newClass="'size-5 m-2'" :color="'white'" @click="toogle"></Icon>
     <a href="#" class="text-2xl font-bold">FitSpot</a>
 
   </nav>
